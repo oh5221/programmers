26f94972ff6e070686359d107ee932ff650c59b0
oh5221 <51361167+oh5221@users.noreply.github.com>
2024-04-13 17:09:44 +0900

    [level 0] Title: 암호 해독, Time: 9.73 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,11 +1,10 @@
 class Solution {
     public String solution(String cipher, int code) {
         String answer = "";
-        for (int i=0; i<cipher.length(); i++){
-            if ((i + 1) % code == 0) { 
-                answer = answer + cipher.charAt(i);
-            }
+        for (int i=code-1; i<cipher.length(); i=i+code){
+            answer = answer + cipher.charAt(i);
         }
+           
         return answer;
     }
 }
