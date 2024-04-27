0d536d0d9ab622aec362996de1c2f7dbac527f4a
oh5221 <51361167+oh5221@users.noreply.github.com>
2024-04-27 19:27:35 +0900

    Update 가장 큰 수.py

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -19,3 +19,44 @@ def solution(numbers):
         return '0'
     else:
         return answer
+
+
+    
+    '''  
+    # 하나씩 확인하면서 a+b가 b+a보다 작으면 swap
+    # 한 번의 for문으로 정렬이 되지 않고 시간이 오래 걸리므로 폐기
+    for i in range(len(numbers) - 1):
+        if numbers[i] + numbers[i+1] <= numbers[i+1] + numbers[i]:
+            numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+    
+    # print(numbers)
+    
+    # answer에 숫자 차례대로 넣기
+    for num in numbers:
+        answer += num
+    '''
+    
+    '''
+     # numbers = sorted(numbers) 기준 앞뒤 숫자로 비교한 코드
+    for i in range(len(numbers)):
+        if (answer + numbers[i]) >= (numbers[i] + answer):
+            answer = (answer + numbers[i])
+            # print(answer)
+        else:
+            answer = (numbers[i] + answer)
+            # print(answer)
+    
+    '''
+    
+
+'''
+# 순열 사용한 풀이 -> 출력 초과로 폐기
+from itertools import permutations
+def solution(numbers):
+    nums = list(map(str, numbers))
+    # print(nums)
+    
+    permu_num = list(map(''.join,permutations(nums, len(nums))))
+    
+    return max(permu_num)
+'''
