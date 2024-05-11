1ec2e0b575cd2a6202847674a83e1e39b53fa06e
oh5221 <51361167+oh5221@users.noreply.github.com>
2024-05-11 15:46:59 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -4,15 +4,8 @@ def solution(n,a,b):
     while a != b:
         # 한번 이기면 4번일 때 2번, 7번일 때 4번
         # 홀수면 +1, 짝수면 // 2
-        if a % 2 != 0:
-            a = a // 2 + 1
-        else:
-            a = a // 2
-            
-        if b % 2 != 0:
-            b = b // 2 + 1
-        else:
-            b = b // 2
+        a = a // 2 + 1 if a % 2 != 0 else a // 2
+        b = b // 2 + 1 if b % 2 != 0 else b // 2
             
         # 한번 이길 때마다 answer++
         answer += 1
