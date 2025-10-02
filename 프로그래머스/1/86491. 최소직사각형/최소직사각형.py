5cceb5c2875054698ccd2049c60db2982eaa934d
oh5221 <51361167+oh5221@users.noreply.github.com>
2025-10-02 19:36:49 +0900

    [level 1] Title: 최소직사각형, Time: 2.39 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,29 +1,15 @@
+# 가로를 무조건 더 큰 숫자가 오도록
+# 작은것중에 큰숫자 * 큰것중에 큰숫자 
 def solution(sizes):
-    # sizes[]의 max를 비교 -> 큰 값을 arr[0]에, 작은 값을 arr[1]에
-    # 각 max값 곱하기
-    cols = len(sizes[0])
-    rows = len(sizes)
-    arr = [[0 for j in range(cols)] for i in range(rows)]
+    answer = 0
+    max_garo, max_sero = 0,0
+    for size in sizes:
+        if size[0] < size[1]:
+            size[0], size[1] = size[1], size[0]
     
-    # [작은값, 큰값] 순으로 정렬하기
-    for i in range(rows):
-        if sizes[i][0] > sizes[i][1]:
-            arr[i][0] = sizes[i][1]
-            arr[i][1] = sizes[i][0]
-        else:
-            arr[i][0] = sizes[i][0]
-            arr[i][1] = sizes[i][1]
-    
-    #print(arr)
-    
-    # arr[][0]의 max1, arr[][1]의 max2
-    max1 = 0
-    max2 = 0
-    for i in range(rows):
-        if max1 < arr[i][0]:
-            max1 = arr[i][0]
-            
-        if max2 < arr[i][1]:
-            max2 = arr[i][1]
-    
-    return max1 * max2
+    for size in sizes:
+        if size[0] > max_garo:
+            max_garo = size[0]
+        if size[1] > max_sero:
+            max_sero = size[1]
+    return max_garo * max_sero
