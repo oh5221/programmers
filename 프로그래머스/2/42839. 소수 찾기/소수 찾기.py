ee2000cb03e72c6b5f60c18acd950fa68fb70706
oh5221 <51361167+oh5221@users.noreply.github.com>
2025-10-02 20:47:28 +0900

    [level 2] Title: 소수 찾기, Time: 2047.45 ms, Memory: 9.57 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,33 +1,25 @@
+# 종이 조각으로 만들 수 있는 소수
 from itertools import permutations
-import math
 def isPrime(num):
     if num < 2:
         return False
-    for i in range(2, int(num**0.5)+1):
+    for i in range(2, num-1):
         if num % i == 0:
             return False
     return True
 def solution(numbers):
-    # 숫자 조각으로 만들 수 있는 수를 구한다
-    # 이 숫자들이 소수인지 아닌지 판별한다
-    count = 0
-    # 중복 없어야 하니까 set
-    nums = set()
-    # numbers
+    answer = 0
     numbers = list(numbers)
-    print(numbers)
+    nums = set()
     
-    # 숫자 조합
-    for i in range(1, len(numbers)+1):
+    for i in range(1, len(numbers) + 1):
         perms = permutations(numbers, i)
         for perm in perms:
             num = int(''.join(perm))
             nums.add(num)
-    print(nums)
+    # print(nums)
     
-    # 소수 체크
     for num in nums:
-        if isPrime(num):
-            count += 1
-            
-    return count
+        if isPrime(num) == True:
+            answer += 1
+    return answer
