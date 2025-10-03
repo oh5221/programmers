bd57747939587b0a8a4845b99a7260d189bc447e
oh5221 <51361167+oh5221@users.noreply.github.com>
2025-10-03 21:13:18 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,6 @@
 def solution(array, commands):
     answer = []
     for command in commands:
-        arr = sorted(array[command[0]-1:command[1]])
-        answer.append(arr[command[2] - 1])
+        result = sorted(array[command[0]-1:command[1]])
+        answer.append(result[command[2]-1])
     return answer
